789aa184514bcbf00dbe5fcd2397e0bb327a2d85
cocktailpeanut <cocktailpeanuts@proton.me>
2025-11-22 01:06:46 -0500

    init

---
+++ pinokio.js
@@ -0,0 +1,53 @@
+module.exports = {
+  title: "Opencode",
+  icon: "icon.png",
+  description: "The AI coding agent built for the terminal.",
+  link: "https://opencode.ai/",
+  install: [{
+    method: "shell.run",
+    params: {
+      message: "npm install -g opencode-ai@latest"
+    }
+  }],
+  uninstall: [{
+    method: "shell.run",
+    params: {
+      message: "npm uninstall -g opencode-ai"
+    }
+  }, {
+    method: "fs.rm",
+    params: {
+      path: "."
+    }
+  }],
+  update: [{
+    method: "shell.run",
+    params: {
+      message: "git pull",
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      message: "npm install -g opencode-ai@latest"
+    }
+  }],
+  run: [{
+    when: "{{platform === 'win32'}}",
+    method: "shell.run",
+    params: {
+      shell: "{{kernel.path('bin/miniconda/Library/bin/bash.exe')}}",
+      message: "opencode",
+      path: "{{args.cwd}}",
+      input: true
+    }
+  }, {
+    when: "{{platform !== 'win32'}}",
+    id: "run",
+    method: "shell.run",
+    params: {
+      message: "opencode",
+      path: "{{args.cwd}}",
+      input: true
+    }
+  }]
+}
